c48dafe3c0fc6b3a15a2192a81095eac626b074d
xiaomingfu <xmingfu86@gmail.com>
2018-11-07 21:45:22 -0800

    js level one and two test

---
+++ js_Level_One.js
@@ -0,0 +1,4 @@
+alert("Welcome to your Bank!");
+var deposit = prompt("How much would you like to deposit?");
+alert("You've deposit " + deposit);
+console.log("You are cool person.");
---
+++ js_Level_One_part_two.js
@@ -0,0 +1,4 @@
+var weightInPounds= prompt("What is your weight in pounds(lbs)?");
+var weightInKilograms = weightInPounds * 0.454;
+alert("Your weight is " + weightInKilograms);
+console.log("Conversion Completed");
